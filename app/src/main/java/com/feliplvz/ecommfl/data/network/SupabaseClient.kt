546dbafd25e39a7bb11dc9c764b72794c905ce9d
Felipe <felipelopez.valenzuela@gmail.com>
2025-11-10 19:57:47 -0300

    feat: create Supabase client

---
+++ app/src/main/java/com/feliplvz/ecommfl/data/network/SupabaseClient.kt
@@ -0,0 +1,17 @@
+package com.feliplvz.ecommfl.data.network
+
+import com.feliplvz.ecommfl.BuildConfig
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+
+object SupabaseClient {
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_ANON_KEY
+    ) {
+        install(Auth)
+        install(Postgrest)
+    }
+}
+
