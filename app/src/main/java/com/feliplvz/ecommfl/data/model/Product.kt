221399366ee33a681a5dae9ae67dac1130ebe80e
Felipe <felipelopez.valenzuela@gmail.com>
2025-11-10 20:15:17 -0300

    refactor: add serial supp to product model (supabase)

--- app/src/main/java/com/feliplvz/ecommfl/data/model/Product.kt
+++ app/src/main/java/com/feliplvz/ecommfl/data/model/Product.kt
@@ -1,18 +1,22 @@
 package com.feliplvz.ecommfl.data.model
 
 import com.feliplvz.ecommfl.data.local.ProductEntity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Product(
     val id: Long = 0,
     val name: String,
     val description: String,
     val price: Double,
+    @SerialName("image_url")
     val imageUrl: String,
     val stock: Int,
     val category: String
 )
 
-// Funciones de mapeo
+// Funciones de mapeo para Room (compatibilidad)
 fun ProductEntity.toProduct() = Product(
     id = id,
     name = name,
