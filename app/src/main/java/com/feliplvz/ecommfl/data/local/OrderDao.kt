dd13633ba4bbd1e32ea4488f90e2628c2d8af245
Felipe <felipelopez.valenzuela@gmail.com>
2025-11-12 19:28:57 -0300

    feat: add queries for users and guest orders

--- app/src/main/java/com/feliplvz/ecommfl/data/local/OrderDao.kt
+++ app/src/main/java/com/feliplvz/ecommfl/data/local/OrderDao.kt
@@ -10,6 +10,12 @@ interface OrderDao {
     @Query("SELECT * FROM orders ORDER BY orderDate DESC")
     fun getAllOrders(): Flow<List<OrderEntity>>
 
+    @Query("SELECT * FROM orders WHERE userId = :userId ORDER BY orderDate DESC")
+    fun getOrdersByUserId(userId: String): Flow<List<OrderEntity>>
+
+    @Query("SELECT * FROM orders WHERE userId IS NULL ORDER BY orderDate DESC")
+    fun getGuestOrders(): Flow<List<OrderEntity>>
+
     @Query("SELECT * FROM orders WHERE id = :orderId")
     suspend fun getOrderById(orderId: Long): OrderEntity?
 
